77ba57be9cbc07ef0f2f404bcd29aa2332b005e4
“Aldahir” <ronn.zp.3@gmail.com>
2024-02-18 21:25:00 -0500

    tipos: -any, unknown -union, interseccion -literal

--- Curso-Microsoft/Introduccion/build/module02.js
+++ Curso-Microsoft/Introduccion/build/module02.js
@@ -25,3 +25,31 @@ var ContractStatus;
 })(ContractStatus || (ContractStatus = {}));
 let employeeStatus = ContractStatus.Temp;
 console.log(employeeStatus);
+// Tipo any, unknown 
+let randomValue = 10;
+randomValue = 'Mateo'; // OK
+randomValue = true; // OK
+console.log(randomValue.name); // Logs "undefined" to the console
+randomValue(); // Returns "randomValue is not a function" error
+randomValue.toUpperCase(); // Returns "randomValue is not a function" error
+// Al se de tipo unknown este no puede acceder a sus propiedades, llamarse como funcion o contructor
+let randomValue2 = 10;
+randomValue2 = true;
+randomValue2 = 'Mateo';
+// console.log(randomValue2.name);  // Error: Object is of type unknown
+// randomValue2();                  // Error: Object is of type unknown
+// randomValue2.toUpperCase();      // Error: Object is of type unknown
+// Tipos UNION, INTERSECCION
+// UNION
+let multiType;
+multiType = 20; //* Valid
+multiType = true; //* Valid
+let newManager = {
+    employeeID: 12345,
+    age: 34,
+    stockPlan: true
+};
+let myResult;
+myResult = "incomplete"; //* Valid
+myResult = "pass"; //* Valid
+// myResult = "failure";       //* Invalid
--- Curso-Microsoft/Introduccion/module02.ts
+++ Curso-Microsoft/Introduccion/module02.ts
@@ -30,3 +30,61 @@ enum ContractStatus {
 
 let employeeStatus: ContractStatus = ContractStatus.Temp;
 console.log(employeeStatus);
+
+
+
+// Tipo any, unknown 
+let randomValue: any = 10;
+randomValue = 'Mateo';   // OK
+randomValue = true;      // OK
+
+console.log(randomValue.name);  // Logs "undefined" to the console
+randomValue();                  // Returns "randomValue is not a function" error
+randomValue.toUpperCase();      // Returns "randomValue is not a function" error
+
+
+// Al se de tipo unknown este no puede acceder a sus propiedades, llamarse como funcion o contructor
+let randomValue2: unknown = 10;
+randomValue2 = true;
+randomValue2 = 'Mateo';
+
+// console.log(randomValue2.name);  // Error: Object is of type unknown
+// randomValue2();                  // Error: Object is of type unknown
+// randomValue2.toUpperCase();      // Error: Object is of type unknown
+
+
+
+
+// Tipos UNION, INTERSECCION
+// UNION
+let multiType: number | boolean;
+multiType = 20;         //* Valid
+multiType = true;       //* Valid
+// multiType = "twenty";   //* Invalid
+
+
+interface Employee {
+    employeeID: number;
+    age: number;
+}
+interface Manager {
+    stockPlan: boolean;
+}
+// INTERSECCION '&'  ==>  combina dos tipos para generar uno que posea ambas caracteristicas
+type ManagementEmployee = Employee & Manager;
+let newManager: ManagementEmployee = {
+    employeeID: 12345,
+    age: 34,
+    stockPlan: true
+};
+
+
+
+
+// Tipos literales
+// Un literal es un subtipo mas concreto de un tipo colectivo
+type testResult = "pass" | "fail" | "incomplete";
+let myResult: testResult;
+myResult = "incomplete";    //* Valid
+myResult = "pass";          //* Valid
+// myResult = "failure";       //* Invalid
